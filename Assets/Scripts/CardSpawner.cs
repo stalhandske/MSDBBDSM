84fbaeb6cba91ac53e7bf6d76ac4903779a00b13
Simon Stålhandske <simon.stalhandske@gmail.com>
2017-06-10 15:19:10 +0200

    Added fonts Changed prefab Made sub review scene made card spawner

---
+++ Assets/Scripts/CardSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSpawner : MonoBehaviour {
+
+
+
+    [Header("References")]
+    public Transform cardParent;
+
+    [Header("Prefabs")]
+    public GameObject cardPrefab;
+    
+	void Start ()
+    {
+        InstantiateDeck(Deck.deckActions);
+        InstantiateDeck(Deck.deckBondages);
+        InstantiateDeck(Deck.deckTouches);
+    }
+
+    void InstantiateDeck(List<CardData> deck)
+    {
+        Debug.Log(deck.Count);
+        foreach(CardData card in deck)
+        {
+            GameObject cardGO = Instantiate(cardPrefab, cardParent);
+            cardGO.GetComponent<CardView>().SetCardView(card);
+        }
+    }
+    
+}
