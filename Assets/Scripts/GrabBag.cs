4a53fd9be954ddd6daf4fe53c82fd5e7003d4795
Simon Stålhandske <simon.stalhandske@gmail.com>
2017-06-10 13:12:28 +0200

    Library Manager and Deck static classes

---
+++ Assets/Scripts/GrabBag.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GrabBag {
+
+    int memberCount;
+    List<int> members;
+
+    public GrabBag(int count)
+    {
+        memberCount = count;
+        members = new List<int>();
+        PopulateBag();
+    }
+
+    void PopulateBag()
+    {
+        for (int i = 0; i < memberCount; i++)
+        {
+            members.Add(i);
+        }
+    }
+
+    public int Grab()
+    {
+        int i = Random.Range(0, members.Count);
+        int result = members[i];
+        members.RemoveAt(i);
+
+        if (members.Count == 0)
+        {
+            PopulateBag();
+        }
+
+        return result;
+    }
+}
---
+++ Assets/Scripts/TestScripts/TestLibrary.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLibrary : MonoBehaviour {
+
+	void Start ()
+    {
+        foreach (CardData card in LibraryManager.DrawActions(4))
+        {
+            Debug.Log(card.title);
+            Deck.SubmitAction(card);
+        }
+
+        foreach (CardData card in LibraryManager.DrawTouches(2))
+        {
+            Debug.Log(card.title);
+            Deck.SubmitTouch(card);
+        }
+
+        foreach (CardData card in LibraryManager.DrawBondages(2))
+        {
+            Debug.Log(card.title);
+            Deck.SubmitBondage(card);
+        }
+
+        foreach (CardData card in Deck.deckActions)
+        {
+            Debug.Log(card.title);
+        }
+
+        foreach (CardData card in Deck.deckBondages)
+        {
+            Debug.Log(card.title);
+        }
+
+        foreach (CardData card in Deck.deckTouches)
+        {
+            Debug.Log(card.title);
+        }
+    }
+}
