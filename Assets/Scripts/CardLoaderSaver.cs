6d497b18c6243d4555fa8e700016b518caf22cc4
Simon Stålhandske <simon.stalhandske@gmail.com>
2017-06-10 11:57:21 +0200

    Initial commit

---
+++ Assets/Scripts/CardLoaderSaver.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public static class CardLoaderSaver
+{
+
+    public static LibraryData LoadLibrary()
+    {
+        LibraryData libraryData;
+
+        string path = GetPath();
+
+        if (!File.Exists(path))
+            return null;
+
+        using (FileStream fs = new FileStream(path, FileMode.Open))
+        {
+            using (StreamReader writer = new StreamReader(fs))
+            {
+                string fromFile = writer.ReadToEnd();
+                //Debug.Log(fromFile);
+                libraryData = JsonUtility.FromJson<LibraryData>(fromFile);
+            }
+        }
+
+        return libraryData;
+    }
+
+    public static void SaveLibrary(LibraryData libraryData)
+    {
+        Directory.CreateDirectory(GetDirectory());
+
+        using (FileStream fs = new FileStream(GetPath(), FileMode.Create))
+        {
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                writer.Write(JsonUtility.ToJson(libraryData,true));
+            }
+        }
+    }
+
+    static string GetPath()
+    {
+        return  GetDirectory() + "/cards" + ".json";
+    }
+
+    static string GetDirectory()
+    {
+        return Application.persistentDataPath;
+    }
+}
---
+++ Assets/Scripts/CreateLibrary.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateLibrary : MonoBehaviour {
+
+	
+	void Start ()
+    {
+        LibraryData libraryData = new LibraryData();
+        libraryData.actionCards = new List<CardData>();
+        libraryData.bondageCards = new List<CardData>();
+        libraryData.touchCards = new List<CardData>();
+
+        CardData cardData = new CardData();
+        cardData.title = "title text";
+        cardData.text = "action text";
+        cardData.intensity = 0;
+        cardData.flavor = "flavor text";
+
+        libraryData.actionCards.Add(cardData);
+
+
+        cardData = new CardData();
+        cardData.title = "title text";
+        cardData.text = "touch text";
+        cardData.intensity = 0;
+        cardData.flavor = "flavor text";
+
+        libraryData.touchCards.Add(cardData);
+
+
+        cardData = new CardData();
+        cardData.title = "title text";
+        cardData.text = "bondage text";
+        cardData.intensity = 0;
+        cardData.flavor = "flavor text";
+
+        libraryData.bondageCards.Add(cardData);
+
+        CardLoaderSaver.SaveLibrary(libraryData);
+    }
+
+}
