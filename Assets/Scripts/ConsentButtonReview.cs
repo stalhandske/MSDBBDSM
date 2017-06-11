ce626d559b419bf92b353f5aa337fae34b8e29b8
Simon Stålhandske <simon.stalhandske@gmail.com>
2017-06-11 16:06:28 +0200

    dynamic text on last screen

---
+++ Assets/Scripts/ConsentButtonReview.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsentButtonReview : MonoBehaviour {
+
+    Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+
+        text.text = GameManager.Instance.SubViewDone ? "Reset game" : "Accept";
+    }
+}
