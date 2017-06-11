d6c8f0271254d4b500e4a8b45e55f51a19f395c3
Simon Stålhandske <simon.stalhandske@gmail.com>
2017-06-11 16:02:32 +0200

    Changed the subreview

---
+++ Assets/Scripts/ReviewTitle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReviewTitle : MonoBehaviour {
+
+    Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+
+        text.text = GameManager.Instance.SubViewDone ? "Chosen cards" : "Review cards";
+    }
+	
+}
